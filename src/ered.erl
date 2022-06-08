a54c4b231d007b83e838916b92cbc4d8e54a2702
Patrik Sellin <patrik.sellin@ericsson.com>
2022-06-08 09:37:13 +0200

    export types from ered.erl

--- src/ered.erl
+++ src/ered.erl
@@ -21,6 +21,13 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3, format_status/2]).
 
+-export_type([opt/0,
+              command/0,
+              reply/0,
+              reply_fun/0,
+              client_ref/0,
+              server_ref/0]).
+
 %%%===================================================================
 %%% Definitions
 %%%===================================================================
