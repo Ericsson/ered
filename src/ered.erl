12112b0ac9add10b73d2e438058d033eb5c6d946
Viktor Söderqvist <viktor.soderqvist@est.tech>
2024-02-21 14:08:44 +0100

    Fixup: formatting of comments

--- src/ered.erl
+++ src/ered.erl
@@ -35,9 +35,12 @@
 %%%===================================================================
 
 -record(st, {cluster_pid :: pid(),
-             slots :: binary(),       % The byte att offset N is an index into
-                                                % the clients tuple for slot N.
-             clients = {} :: tuple(), % ... of pid() (or addr() as placeholder)
+             slots :: binary(),                 % The byte att offset N is an
+                                                % index into the clients tuple
+                                                % for slot N.
+             clients = {} :: tuple(),           % Tuple of pid(), or addr() as
+                                                % placeholder when the process
+                                                % is gone.
              slot_map_version = 0 :: non_neg_integer(),
              addr_map = #{} :: #{addr() => pid() | Placeholder :: addr()},
              pending = #{} :: #{gen_server:from() => pid()},
