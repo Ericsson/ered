c700043ca2c64bc73a967643b9b15f83a2e61b7a
Hippo <44473866+WilliamVoong@users.noreply.github.com>
2026-03-17 11:00:24 +0100

    Add support for sending commands in batches (#158)
    When the pending queue is full, don't send more commands until the
    pending queue can fit another batch of commands, as configured with the
    existing config `batch_size`.
    
    The purpose is to improve performance by sending fewer and larger TCP
    packets.
    
    The config for `batch_size` has existed before but the batching
    functionality has not worked properly.
    
    Bonus: Use assert macros instead of direct pattern matching in eunit
    tests to verify output/input, to get better error messages on failure.
    
    ---------
    
    Co-authored-by: William Voong <william.voong@ericsson.com>
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- test/ered_client_tests.erl
+++ test/ered_client_tests.erl
@@ -11,6 +11,7 @@ run_test_() ->
      {spawn, fun server_close_socket_t/0},
      {spawn, fun bad_request_t/0},
      {spawn, fun server_buffer_full_t/0},
+     {spawn, fun low_high_watermark_t/0},
      {spawn, fun bad_option_t/0},
      {spawn, fun bad_connection_option_t/0},
      {spawn, fun server_buffer_full_reconnect_t/0},
@@ -118,7 +119,7 @@ server_buffer_full_t() ->
                        Expected = iolist_to_binary(lists:duplicate(5, Ping)),
                        {ok, Expected} = gen_tcp:recv(Sock, size(Expected)),
                        %% should be nothing more since only 5 pending
-                       {error, timeout} = gen_tcp:recv(Sock, 0, 0),
+                       ?assertEqual({error, timeout}, gen_tcp:recv(Sock, 0, 0)),
 
                        timer:sleep(500),
 
@@ -138,11 +139,61 @@ server_buffer_full_t() ->
     Pid = self(),
     [ered_client:command_async(Client, [<<"ping">>], fun(Reply) -> Pid ! {N, Reply} end) || N <- lists:seq(1,11)],
     receive #{msg_type := queue_full} -> ok end,
-    {6, {error, queue_overflow}} = get_msg(),
+    ?assertMatch({6, {error, queue_overflow}}, get_msg()),
     receive #{msg_type := queue_ok} -> ok end,
-    [{N, {ok, <<"pong">>}} = get_msg()|| N <- [1,2,3,4,5,7,8,9,10,11]],
+    [?assertMatch({N, {ok, <<"pong">>}}, get_msg()) || N <- [1,2,3,4,5,7,8,9,10,11]],
     no_more_msgs().
 
+low_high_watermark_t() ->
+    {ok, ListenSock} = gen_tcp:listen(0, [binary, {active, false}]),
+    {ok, Port} = inet:port(ListenSock),
+    ServerPid = spawn_link(fun() ->
+                                   {ok, Sock} = gen_tcp:accept(ListenSock),
+                                   Ping = <<"*1\r\n$4\r\nping\r\n">>,
+                                   FivePing = iolist_to_binary(lists:duplicate(5, Ping)),
+                                   {ok, FivePing} = gen_tcp:recv(Sock, size(FivePing)),
+
+                                   %% should be nothing more since only 5 pending
+                                   ?assertEqual({error, timeout}, gen_tcp:recv(Sock, 0, 0)),
+
+                                   gen_tcp:send(Sock, lists:duplicate(4, <<"+pong\r\n">>)),
+                                   receive send_one_more_pong -> ok end,
+                                   gen_tcp:send(Sock, lists:duplicate(1, <<"+pong\r\n">>)),
+
+                                   {ok, FivePing} = gen_tcp:recv(Sock, 0),
+                                   gen_tcp:send(Sock, lists:duplicate(5, <<"+pong\r\n">>)),
+                                   receive ok -> ok end
+                           end),
+    Client = start_client(Port, [{connection_opts, [{batch_size,5}]}, {max_waiting, 10}, {max_pending, 5}, {queue_ok_level,1}]),
+    expect_connection_up(Client),
+
+    Pid = self(),
+    [ered_client:command_async(Client, [<<"ping">>], fun(Reply) -> Pid ! {N, Reply} end) || N <- lists:seq(1,10)],
+    [?assertEqual({N, {ok, <<"pong">>}}, get_msg()) || N <- [1,2,3,4]],
+
+    %% high water mark is hit, and we can not fill more until we reach low water-mark.
+    ?assertMatch(#{pending := {1, _},
+                   waiting := {5, _},
+                   filling_batch := false},
+                 ered_client:state_to_map(sys:get_state(Client))),
+
+    ServerPid ! send_one_more_pong,
+    [?assertEqual({N, {ok, <<"pong">>}}, get_msg()) || N <- [5]],
+
+    %% low water mark reached, pending should now be filled.
+    ?assertMatch(#{pending := {5, _},
+                   waiting := {0, _},
+                   filling_batch := false},
+                 ered_client:state_to_map(sys:get_state(Client))),
+
+    [?assertEqual({N, {ok, <<"pong">>}}, get_msg()) || N <- [6,7,8,9,10]],
+
+    ?assertMatch(#{pending := {0, _},
+                   waiting := {0, _},
+                   filling_batch := true},
+                 ered_client:state_to_map(sys:get_state(Client))),
+
+    no_more_msgs().
 
 
 server_buffer_full_reconnect_t() ->
@@ -150,17 +201,16 @@ server_buffer_full_reconnect_t() ->
     {ok, Port} = inet:port(ListenSock),
     spawn_link(fun() ->
                        {ok, Sock} = gen_tcp:accept(ListenSock),
-                       %% expect 5 ping
                        Ping = <<"*1\r\n$4\r\nping\r\n">>,
-                       Expected = iolist_to_binary(lists:duplicate(5, Ping)),
-                       {ok, Expected} = gen_tcp:recv(Sock, size(Expected)),
+                       FivePing = iolist_to_binary(lists:duplicate(5, Ping)),
+                       {ok, FivePing} = gen_tcp:recv(Sock, size(FivePing)),
                        %% should be nothing more since only 5 pending
                        {error, timeout} = gen_tcp:recv(Sock, 0, 0),
 
                        gen_tcp:close(Sock),
 
                        {ok, Sock2} = gen_tcp:accept(ListenSock),
-                       {ok, Expected} = gen_tcp:recv(Sock2, size(Expected)),
+                       {ok, FivePing} = gen_tcp:recv(Sock2, size(FivePing)),
 
                        gen_tcp:send(Sock2, lists:duplicate(5, <<"+pong\r\n">>)),
                        %% should be nothing more since only 5 pending
@@ -192,10 +242,9 @@ server_buffer_full_node_goes_down_t() ->
     {ok, Port} = inet:port(ListenSock),
     spawn_link(fun() ->
                        {ok, Sock} = gen_tcp:accept(ListenSock),
-                       %% expect 5 ping
                        Ping = <<"*1\r\n$4\r\nping\r\n">>,
-                       Expected = iolist_to_binary(lists:duplicate(5, Ping)),
-                       {ok, Expected} = gen_tcp:recv(Sock, size(Expected)),
+                       FivePing = iolist_to_binary(lists:duplicate(5, Ping)),
+                       {ok, FivePing} = gen_tcp:recv(Sock, size(FivePing)),
                        %% should be nothing more since only 5 pending
                        {error, timeout} = gen_tcp:recv(Sock, 0, 0),
                        gen_tcp:close(ListenSock)
@@ -206,9 +255,9 @@ server_buffer_full_node_goes_down_t() ->
     Pid = self(),
     [ered_client:command_async(Client, [<<"ping">>], fun(Reply) -> Pid ! {N, Reply} end) || N <- lists:seq(1,11)],
     receive #{msg_type := queue_full} -> ok end,
-    {6, {error, queue_overflow}} = get_msg(),
+    ?assertEqual({6, {error, queue_overflow}}, get_msg()),
     receive #{msg_type := socket_closed, reason := tcp_closed} -> ok end,
-    [{N, {error, queue_overflow}} = get_msg() || N <- [1,2,3,4,5]],
+    [?assertEqual({N, {error, queue_overflow}}, get_msg()) || N <- [1,2,3,4,5]],
     receive #{msg_type := queue_ok} -> ok end,
     receive #{msg_type := connect_error, reason := econnrefused} -> ok end,
     receive #{msg_type := node_down_timeout} -> ok end,
